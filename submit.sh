eda9270b8b314e628201419af3d748ad0dbc5eee
harris-mj <mjh285@cantab.ac.uk>
2025-11-03 14:08:04 +0000

    added mace-scripts as submodule

--- submit.sh
+++ submit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 source config.cfg
+source ~/.bashrc
 bash make_lammpscfg.sh
 mkdir -p dump
 mkdir -p Slurm
 mv slurm-* Slurm/ 2>/dev/null || true
-sbatch --job-name="${job_name}" run.sbatch
+sbatch --job-name="${job_name}" run.${HPC}.sbatch
